04ae65f08b31a8c50bfe74b36745bc21d2bb787f
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2023-06-27 16:03:46 +0530

    ADD: tailwind

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+export default {}
+</script>
+
+
+
+<template>
+
+  <div class="flex flex-row font-poppins">
+
+    <div class='sidebar hidden sm:block'>
+      <Sidebar />
+    </div>
+    <div class="w-full p-5">
+      <router-view />
+      <!-- <div class="app-bar block md:hidden">
+        <v-app-bar
+          class="hidden"
+          elevation="4"
+        ></v-app-bar>
+      </div> -->
+      <!-- <div class="dashboard p-5">
+        <Dashboard />
+      </div> -->
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
