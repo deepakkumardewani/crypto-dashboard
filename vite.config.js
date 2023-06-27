0e6bb106987ab93afc5ead99e23b551ea659110f
Deepak Kumar Dewani <kumard8308887772@gmail.com>
2023-06-27 16:04:29 +0530

    ADD: vite

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { fileURLToPath } from 'url'
+import { defineConfig } from 'vite'
+import { createVuePlugin as vue2 } from 'vite-plugin-vue2'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue2({
+      jsx: true
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      vue: 'vue/dist/vue.esm.js'
+    }
+  }
+})
